5789f6ced511e525b35e3506c073d6f1a6392d36
snakeful <fulblind@gmail.com>
2021-10-10 22:52:48 -0600

    :white_check_mark: update decorator test

---
+++ packages/orm/test/orm.test.ts
@@ -0,0 +1,26 @@
+import { Model } from '../src';
+import { Field, Id, Table } from '../src/decorators';
+
+@Table('test', 'schema')
+class TestModel extends Model {
+  @Id()
+  id!: number;
+  @Field()
+  name!: string;
+}
+
+describe('decorators', () => {
+  it('Should assign all table related decorators', () => {
+    expect(TestModel.table).toBe('test');
+    expect(TestModel.schema).toBe('schema');
+    expect(TestModel.primaryKeys).toBeInstanceOf(Array);
+    expect(TestModel.primaryKeys[0].name).toBe('id');
+    expect(TestModel.primaryKeys[0].type).toBe('number');
+    expect(TestModel.fields).toBeInstanceOf(Array);
+    expect(TestModel.fields.length).toBe(2);
+    expect(TestModel.fields[0].name).toBe('id');
+    expect(TestModel.fields[0].type).toBe('number');
+    expect(TestModel.fields[1].name).toBe('name');
+    expect(TestModel.fields[1].type).toBe('string');
+  });
+});
