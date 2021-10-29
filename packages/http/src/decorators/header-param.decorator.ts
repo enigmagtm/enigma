86475de9a8fe395887996fa3af71c5176715ba28
snakeful <fulblind@gmail.com>
2021-10-29 13:12:28 -0600

    :art: improve code

--- packages/http/src/decorators/header-param.decorator.ts
+++ packages/http/src/decorators/header-param.decorator.ts
@@ -1,10 +1,10 @@
-import { HeaderParameter, HEADER_PARAMS, METHOD } from '../types';
+import { HEADER_PARAMS, METHOD, Parameter } from '../types';
 
 export const HeaderParam = (name: string): ParameterDecorator => {
   const HeaderParamDecorator = (target: Object, property: string | symbol, index: number): void => {
     const metadataKey = `${METHOD}${HEADER_PARAMS}${String(property)}`;
-    const params: HeaderParameter[] = Reflect.getOwnMetadata(metadataKey, target) || [];
-    if (params.findIndex((param: HeaderParameter) => param.name === property) === -1) {
+    const params: Parameter[] = Reflect.getOwnMetadata(metadataKey, target) || [];
+    if (params.findIndex((param: Parameter) => param.name === property) === -1) {
       params.unshift({ name, index });
     }
     Reflect.defineMetadata(metadataKey, params, target);
