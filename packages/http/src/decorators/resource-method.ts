182348f0bf9354f77994d858ad8916e76f7efeb9
snakeful <fulblind@gmail.com>
2021-10-29 16:38:31 -0600

    :art: improve code

--- packages/http/src/decorators/resource-method.ts
+++ packages/http/src/decorators/resource-method.ts
@@ -2,16 +2,16 @@ import { NextFunction, Request, RequestHandler, Response } from '@enigmagtm/core
 import { BODY_PARAM, CustomHttpMethod, HEADER_PARAMS, METHOD, Parameter, PATH_PARAMS, QueryParameter, QUERY_PARAMS } from '../types';
 
 export interface ResourceParameters {
-  headerParamsDef: Parameter[];
-  pathParamsDef: Parameter[];
-  queryParamsDef: QueryParameter[];
-  bodyParamDef: Parameter;
+  headerParamsDef?: Parameter[];
+  pathParamsDef?: Parameter[];
+  queryParamsDef?: QueryParameter[];
+  bodyParamDef?: Parameter;
 }
 
 export const getResourceParameters = (target: Object, property: string): ResourceParameters => {
-  const headerParamsDef: Parameter[] = Reflect.getOwnMetadata(`${METHOD}${HEADER_PARAMS}${String(property)}`, target) || [];
-  const pathParamsDef: Parameter[] = Reflect.getOwnMetadata(`${METHOD}${PATH_PARAMS}${String(property)}`, target) || [];
-  const queryParamsDef: QueryParameter[] = Reflect.getOwnMetadata(`${METHOD}${QUERY_PARAMS}${String(property)}`, target) || [];
+  const headerParamsDef: Parameter[] = Reflect.getOwnMetadata(`${METHOD}${HEADER_PARAMS}${String(property)}`, target);
+  const pathParamsDef: Parameter[] = Reflect.getOwnMetadata(`${METHOD}${PATH_PARAMS}${String(property)}`, target);
+  const queryParamsDef: QueryParameter[] = Reflect.getOwnMetadata(`${METHOD}${QUERY_PARAMS}${String(property)}`, target);
   const bodyParamDef: Parameter = Reflect.getOwnMetadata(`${METHOD}${BODY_PARAM}${String(property)}`, target);
   return { headerParamsDef, pathParamsDef, queryParamsDef, bodyParamDef };
 }
@@ -20,9 +20,9 @@ export const createResourceMethod = (target: any, httpMethod: CustomHttpMethod, 
   return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     req = (req as any);
     const { headerParamsDef, pathParamsDef, queryParamsDef, bodyParamDef } = params;
-    const headerParams = headerParamsDef.map((param: Parameter) => ({ ...param, value: req.get(param.name) }));
-    const pathParams = pathParamsDef.map((param: Parameter) => ({ ...param, value: req.params[param.name] }));
-    const queryParams = queryParamsDef.map((param: Parameter) => ({ ...param, value: req.query[param.name] }));
+    const headerParams = headerParamsDef?.map((param: Parameter) => ({ ...param, value: req.get(param.name) })) || [];
+    const pathParams = pathParamsDef?.map((param: Parameter) => ({ ...param, value: req.params[param.name] })) || [];
+    const queryParams = queryParamsDef?.map((param: Parameter) => ({ ...param, value: req.query[param.name] })) || [];
     const orderedParams: Parameter[] = [...headerParams, ...pathParams, ...queryParams];
     if (bodyParamDef) {
       orderedParams.push(bodyParamDef);
