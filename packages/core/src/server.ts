eee33dcf6f1f4c898965d8a5c1c61fb91cac3c94
snakeful <fulblind@gmail.com>
2021-11-01 00:10:33 -0600

    :sparkles: add boostrap for enigma core

--- packages/core/src/server.ts
+++ packages/core/src/server.ts
@@ -1,17 +1,34 @@
 import express from 'express';
 import { NextFunction, Request, Response, TypeRef } from './types';
 
-export const boostrapModule = (module: TypeRef<any>, port: number, poweredBy?: boolean): void => {
-  const server = express();
-  if (poweredBy) server.disable('x-powered-by');
-  server.listen(port, () => console.log(`Server running on port ${port}`))
-  new module(server);
-  server.get('*', (_req: Request, res: Response, next: NextFunction): Promise<Response | void> | Response | void => {
-    if (!res.headersSent) {
-      return res.status(404).json({
-        err: 'Url not found on server'
-      });
+interface ServerOptions {
+  port?: number;
+  poweredBy?: boolean;
+}
+
+type BootstrapModule = (module: TypeRef<any>, options?: ServerOptions) => void;
+export interface EnigmaServer {
+  bootstrap: BootstrapModule;
+}
+
+export const enigmaServer: EnigmaServer = {
+  bootstrap(module: TypeRef<any>, options: ServerOptions = {}): void {
+    const { port = 36442, poweredBy = false } = options;
+    const server = express();
+    if (poweredBy) {
+      server.disable('x-powered-by');
     }
-    return next();
-  });
+
+    server.listen(port, () => console.log(`Server running on port ${port}`))
+    new module(server);
+    server.get('*', (_req: Request, res: Response, next: NextFunction): Promise<Response | void> | Response | void => {
+      if (!res.headersSent) {
+        return res.status(404).json({
+          err: 'Url not found on server'
+        });
+      }
+
+      return next();
+    });
+  }
 };
--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -1,4 +1,4 @@
 export * from './e-types';
 export * from './property';
-export * from './base-record';
+export * from './table';
 export * from './types';
