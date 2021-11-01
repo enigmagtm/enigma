27a26751e44ed896bad55aae313963d6d361ee97
snakeful <fulblind@gmail.com>
2021-11-01 03:30:49 -0600

    :art: add credentials interface

---
+++ packages/http/src/types/credentials.ts
@@ -0,0 +1,6 @@
+import { BaseRecord } from '@enigmagtm/core';
+
+export interface Credentials {
+  user: BaseRecord<number>;
+  token: any;
+}
--- packages/http/src/types/index.ts
+++ packages/http/src/types/index.ts
@@ -1,5 +1,6 @@
+export * from './credentials';
 export * from './header-type';
 export * from './http-error';
 export * from './http-status';
-export * from './http-verb';
 export * from './http-types';
+export * from './http-verb';
