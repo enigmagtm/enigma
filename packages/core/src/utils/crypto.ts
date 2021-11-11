f2222000b9abe11ab3602d215f575440e842c578
snakeful <fulblind@gmail.com>
2021-11-11 13:11:18 -0600

    :art: improve code

--- packages/core/src/utils/crypto.ts
+++ packages/core/src/utils/crypto.ts
@@ -1,29 +1,33 @@
-import { BinaryToTextEncoding, createCipheriv, createDecipheriv, createHash, Encoding, randomBytes } from 'crypto';
+import { BinaryLike, BinaryToTextEncoding, createCipheriv, createDecipheriv, createHash, Encoding, randomBytes } from 'crypto';
+import { isNil } from 'lodash';
 
 export interface Crypto {
-  encrypt(key: string, data: string, algorithm?: string, input?: Encoding, output?: BinaryToTextEncoding): string;
-  decrypt(key: string, data: string, algorithm?: string, input?: BinaryToTextEncoding, output?: Encoding): string;
+  encrypt(key: string, data: string, algorithm?: string, input?: Encoding, output?: BinaryToTextEncoding, iv?: BinaryLike): string;
+  decrypt(key: string, data: string, algorithm?: string, input?: BinaryToTextEncoding, output?: Encoding, iv?: BinaryLike): string;
   randomBytes(bytes?: number): string;
 }
 
 export const crypto: Crypto = {
-  encrypt: (key: string, data: string, algorithm = 'aes256', input: Encoding = 'utf8', output: BinaryToTextEncoding = 'base64'): string => {
-    const iv = Buffer.allocUnsafe(16);
-    const hash = createHash('sha256').update(key.toString()).digest();
-    hash.copy(iv);
-    console.log(iv);
-    const bufferKey = createHash('sha256').update(key).digest();
-    const cryptoCipher = createCipheriv(algorithm, bufferKey, iv);
+  encrypt: (key: string, data: string, algorithm = 'aes256', input: Encoding = 'utf8', output: BinaryToTextEncoding = 'base64', iv?: BinaryLike): string => {
+    const hash = createHash('sha256').update(key).digest();
+    if (isNil(iv)) {
+      iv = Buffer.allocUnsafe(16);
+      hash.copy(iv);
+    }
+
+    const cryptoCipher = createCipheriv(algorithm, hash, iv);
     const result = cryptoCipher.update(data, input, output).concat(cryptoCipher.final(output));
     return result;
   },
 
-  decrypt: (key: string, data: string, algorithm = 'aes256', input: BinaryToTextEncoding = 'base64', output: Encoding = 'utf8'): string => {
-    const iv = Buffer.allocUnsafe(16);
-    const hash = createHash('sha256').update(key.toString()).digest();
-    hash.copy(iv);
-    const bufferKey = createHash('sha256').update(key).digest();
-    const cryptoDecipher = createDecipheriv(algorithm, bufferKey, iv);
+  decrypt: (key: string, data: string, algorithm = 'aes256', input: BinaryToTextEncoding = 'base64', output: Encoding = 'utf8', iv?: BinaryLike): string => {
+    const hash = createHash('sha256').update(key).digest();
+    if (isNil(iv)) {
+      iv = Buffer.allocUnsafe(16);
+      hash.copy(iv);
+    }
+
+    const cryptoDecipher = createDecipheriv(algorithm, hash, iv);
     const result = cryptoDecipher.update(data, input, output).concat(cryptoDecipher.final(output));
     return result;
   },
